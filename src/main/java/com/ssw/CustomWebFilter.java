8bbf2eff0f08acd3bce132c1d3c38fd9d2e427a1
sonseungwoo <skyghy3342@naver.com>
2019-01-31 09:20:22 +0900

    webFilter 추가

---
+++ src/main/java/com/ssw/CustomWebFilter.java
@@ -0,0 +1,25 @@
+package com.ssw;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by Leo.
+ * User: ssw
+ * Date: 2019-01-31
+ * Time: 09:16
+ */
+@Configuration
+public class CustomWebFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        if (exchange.getRequest().getURI().getPath().equals("/")) {
+            return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build()).build());
+        }
+
+        return chain.filter(exchange);
+    }
+}
